d738a19facd8295250e5155318432ebbccb88dc2
susTuna <hidasetsuna@gmail.com>
2025-06-14 21:56:09 +0700

    chore: pdf util now filter non alphanumeric

--- src/database/pdf_utils.py
+++ src/database/pdf_utils.py
@@ -1,5 +1,6 @@
 import PyPDF2
 import os
+import re
 
 def extract_text_from_pdf(pdf_path):
     """Extract text from PDF file using PyPDF2"""
@@ -27,10 +28,13 @@ def prepare_texts_from_pdf(pdf_path: str) -> tuple[str, str] | None:
     
     if not raw_text:
         return None
+    
+    regex_text = raw_text
+    regex_text = re.sub(r'[^a-zA-Z0-9\s.,!?;:\'"`@#$%^&*()_+={}\[\]|\\/<>~-]', ' ', regex_text, flags=re.UNICODE)
 
     text_for_pattern = raw_text.replace('\n', ' ').replace('\r', ' ').strip().lower()
 
-    return raw_text, text_for_pattern
+    return regex_text, text_for_pattern
 
 def save_extracted_texts(pdf_path, output_regex, output_pattern):
     """Extract and save text in two formats: raw and linear"""
@@ -38,20 +42,20 @@ def save_extracted_texts(pdf_path, output_regex, output_pattern):
     os.makedirs(os.path.dirname(output_regex), exist_ok=True)
     os.makedirs(os.path.dirname(output_pattern), exist_ok=True)
     
-    text = extract_text_from_pdf(pdf_path)
+    text = prepare_texts_from_pdf(pdf_path)
     
     if not text:
         print(f"No text extracted from {pdf_path}")
         return False
+    
+    regex_text, pattern_text = text
 
     # Simpan untuk regex (APA ADANYA, persis aslinya)
     with open(output_regex, 'w', encoding='utf-8') as f:
-        f.write(text)
+        f.write(regex_text)
 
-    # Simpan untuk pattern matching (linear, bersih)
-    text_linear = text.replace('\n', ' ').replace('\r', ' ').strip().lower()
     with open(output_pattern, 'w', encoding='utf-8') as f:
-        f.write(text_linear)
+        f.write(pattern_text)
     
     print(f"Text extracted and saved: {output_regex}, {output_pattern}")
     return True
